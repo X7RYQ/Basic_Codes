73bdfcf5587eed31714bb4966eac0816be147afc
X7RYQ <x7ryq.is.cool@gmail.com>
2025-11-09 22:37:50 +0200

    Implement string input and palindrome check
    Added functionality to read a string and check for numbers. Implemented logic to reverse the string and compare it for palindrome check.

--- C++/Sem-1/Hw/Hw3.cpp
+++ C++/Sem-1/Hw/Hw3.cpp
@@ -5,6 +5,42 @@
 using namespace std;
 int main()
 {
+    int size = 0, c, i;
+    std::string str[size], rev[size];
+    char a;
+    cout << "Enter a string with no numbers and no more than 100 symbols: " << endl;
+    cin >> a;
+    while (a != '.')
+    {
+        if (a >= '1' && a <= '9')
+        {
+            cout << "String contains numbers" << endl;
+            cin >> a;
+        }
+        else if (size > 100)
+        {
+            break;
+        }
+        else
+        {
+            str[size] += a;
+            size++;
+            cin >> a;
+        }
+    }
+    c = size - i - 1;
+    for (i = 0; i < size; i++)
+    {
+        rev[c] = str[i];
+    }
+    if (str == rev)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
